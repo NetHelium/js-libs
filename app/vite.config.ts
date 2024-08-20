e9bf4eb204e2f659306cf8e4d2c40346b8832edf
Ronan Henry <135964703+RonanHenry35@users.noreply.github.com>
2024-08-20 17:21:16 +0200

    chore(app): update path aliases

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,7 +5,8 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 export default defineConfig({
   resolve: {
     alias: {
-      $root: resolve(__dirname, "./src"),
+      $root: resolve(__dirname, "./"),
+      $config: resolve(__dirname, "./config"),
       $assets: resolve(__dirname, "./src/assets"),
       $components: resolve(__dirname, "./src/components"),
       $pages: resolve(__dirname, "./src/pages"),
