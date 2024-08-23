e9f73b5a4069126348924754ce972b4b6d45aba5
Ronan Henry <135964703+RonanHenry35@users.noreply.github.com>
2024-08-23 15:53:06 +0200

    chore(app): add page with multiple forms loaded

--- app/src/pages/hc-forms/one-form.ts
+++ app/src/pages/hc-forms/one-form.ts
@@ -3,13 +3,13 @@ import { css, html } from "lit";
 import { customElement } from "lit/decorators.js";
 import "@net-helium/hc-forms";
 
-@customElement("hc-forms--forms-integration")
+@customElement("hc-forms--one-form")
 export default class HcFormsFormsIntegration extends NhPage {
   /**
    * Page metadata.
    */
   static override metadata = {
-    title: "Form integration",
+    title: "One form",
     position: 1,
   };
 
@@ -19,7 +19,7 @@ export default class HcFormsFormsIntegration extends NhPage {
    */
   protected override render() {
     return html`
-      <h1>Form integration</h1>
+      <h1>One form</h1>
       <hc-form
         url="https://helium-connect.fr/player/5b759aaa441eec5fb400002d/fid/66c5dcaab1ff63b9351bd3dc"
       ></hc-form>
