133ce74387a1cf25ac7b8d24107f716a91b79f7a
Henskelis <135964703+RonanHenry35@users.noreply.github.com>
2024-10-14 09:47:52 +0200

    chore: update root configurations and dependency upgrades

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+    coverage: {
+      enabled: true,
+      provider: "v8",
+      reportsDirectory: "./test/.coverage",
+      include: ["src/**"],
+      exclude: ["src/index.ts"],
+    },
+  },
+});
