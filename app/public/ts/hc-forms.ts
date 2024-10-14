eecffa808636249803a21560a138cc3cbdb406e1
Henskelis <135964703+RonanHenry35@users.noreply.github.com>
2024-10-14 09:43:04 +0200

    chore(app): replace eleventy with astro

--- app/public/ts/hc-forms.ts
+++
@@ -1,1 +0,0 @@
-import "@net-helium/hc-forms";
--- app/public/ts/ui/components/icon.ts
+++
@@ -1,4 +0,0 @@
-import { loadIcons, xCircleIcon } from "@net-helium/ui/icons";
-import "@net-helium/ui/components/icon";
-
-loadIcons({ "x-circle": xCircleIcon });
