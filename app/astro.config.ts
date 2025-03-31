d8cd39629182d109ba42c384009d7a745e0599b3
Ronan Henry <135964703+RonanHenry35@users.noreply.github.com>
2025-03-31 10:54:59 +0200

    chore: update dev app

---
+++ app/astro.config.ts
@@ -0,0 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  server: {
+    port: 4321,
+  },
+});
